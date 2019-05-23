6ce8ee90bbd6bcd17874f096cadb8a2828457fcd
gctom <gctom57@gmail.com>
2019-05-23 21:46:11 +0200

    https://www.codingame.com/training/easy/mime-type

---
+++ Easy/MimeType.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt(); // Number of elements which make up the association table.
+        int Q = in.nextInt(); // Number Q of file names to be analyzed.
+
+        Map<String, String> mimeMap = new HashMap();
+        for (int i = 0; i < N; i++) {
+            mimeMap.put(in.next().toUpperCase(), in.next());
+            in.nextLine();
+        }
+
+        for (int i = 0; i < Q; i++) {
+            String FNAME = in.nextLine().toUpperCase(); // One file name per line.
+
+            String extension = "";
+            int index = FNAME.lastIndexOf('.');
+            if (index >= 0) {
+                extension = FNAME.substring(index+1);
+            }
+
+            System.out.println(mimeMap.containsKey(extension) ? mimeMap.get(extension) : "UNKNOWN");
+
+        }
+    }
+}
