37cae9fb0f5548c008d0c4139ee5faca82f87039
gctom <gctom57@gmail.com>
2019-05-23 21:17:23 +0200

    https://www.codingame.com/training/easy/defibrillators

---
+++ Easy/Defibrillators.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        Double longitude = Double.parseDouble(in.next().replaceAll(",", "."));
+        Double latitude = Double.parseDouble(in.next().replaceAll(",", "."));
+
+        int N = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+
+        Double min = Double.MAX_VALUE;
+        String res = "";
+
+        for (int i = 0; i < N; i++) {
+            String DEFIB = in.nextLine();
+            String[] str = DEFIB.split(";");
+            Double defibLon = Double.parseDouble(str[4].replaceAll(",", "."));
+            Double defibLat = Double.parseDouble(str[5].replaceAll(",", "."));
+
+            Double x = (defibLon - longitude) * Math.cos((latitude + defibLat) / 2);
+            Double y = defibLat - latitude;
+            Double d = Math.sqrt(x * x + y * y) * 6371;
+
+            if (d <= min) {
+                res = str[1];
+                min = d;
+            }
+        }
+
+        System.out.println(res);
+    }
+}
