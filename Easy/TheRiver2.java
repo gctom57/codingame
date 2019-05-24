4e64fe753c375ae2f3394c9b74eb2a3db313aea2
gctom <gctom57@gmail.com>
2019-05-24 12:41:52 +0200

    https://www.codingame.com/training/easy/the-river-ii-

---
+++ Easy/TheRiver2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int r1 = in.nextInt();
+
+        boolean found = false;
+        for (int start = r1 - 1; start > 0; start--) {
+            int current = start;
+            current = nextRiver(current);
+            if (current == r1) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static int nextRiver(int n) {
+        int s = n;
+        while (s > 0) {
+            n = n + (s % 10);
+            s = s / 10;
+        }
+        return n;
+    }
+}
