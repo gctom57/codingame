3c99af2dcd9ad29bc15a0e17975733cc91126675
gctom <gctom57@gmail.com>
2019-05-24 12:16:16 +0200

    https://www.codingame.com/training/easy/power-of-thor-episode-1

---
+++ Easy/PowerOfThorEpisode1.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ * ---
+ * Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int lightX = in.nextInt(); // the X position of the light of power
+        int lightY = in.nextInt(); // the Y position of the light of power
+        int initialTX = in.nextInt(); // Thor's starting X position
+        int initialTY = in.nextInt(); // Thor's starting Y position
+
+        // game loop
+        while (true) {
+            int remainingTurns = in.nextInt(); // The remaining amount of turns Thor can move. Do not remove this line.
+
+            String direction = "";
+
+            if (lightY < initialTY) {
+                direction += "N";
+                initialTY--;
+            } else if (lightY > initialTY) {
+                direction += "S";
+                initialTY++;
+            }
+
+            if (lightX < initialTX) {
+                direction += "W";
+                initialTX--;
+            } else if (lightX > initialTX) {
+                direction += "E";
+                initialTX++;
+            }
+
+            System.out.println(direction);
+        }
+    }
+}
