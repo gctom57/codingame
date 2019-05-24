84e3b16ca59876027fc087297d59e74ee77f1b6c
gctom <gctom57@gmail.com>
2019-05-24 12:16:42 +0200

    https://www.codingame.com/training/easy/mars-lander-episode-1

---
+++ Easy/MarsLanderEpisode1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int surfaceN = in.nextInt(); // the number of points used to draw the surface of Mars.
+        for (int i = 0; i < surfaceN; i++) {
+            int landX = in.nextInt(); // X coordinate of a surface point. (0 to 6999)
+            int landY = in.nextInt(); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+        }
+
+        double marsGravity = 3.711;
+
+        // game loop
+        while (true) {
+            int X = in.nextInt();
+            int Y = in.nextInt();
+            int hSpeed = in.nextInt(); // the horizontal speed (in m/s), can be negative.
+            int vSpeed = in.nextInt(); // the vertical speed (in m/s), can be negative.
+            int fuel = in.nextInt(); // the quantity of remaining fuel in liters.
+            int rotate = in.nextInt(); // the rotation angle in degrees (-90 to 90).
+            int power = in.nextInt(); // the thrust power (0 to 4).
+
+            if (vSpeed <= -40) {
+                System.out.println("0 4");
+            } else if (vSpeed <= -30) {
+                System.out.println("0 3");
+            } else {
+                System.out.println("0 0");
+            }
+        }
+    }
+}
