222514cfd7a80bee028296a9879326efc942158f
gctom <gctom57@gmail.com>
2019-05-24 12:27:47 +0200

    https://www.codingame.com/training/easy/xml-mdf-2016

---
+++ Easy/XmlMdf2016.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    private static HashMap<Character, Double> weight = new HashMap<>();
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String sequence = in.nextLine();
+
+        System.err.println(sequence);
+
+        char[] charArray = sequence.toCharArray();
+        int depth = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] == '-') {
+                i++;
+                weight.putIfAbsent(charArray[i], 0d);
+                weight.put(charArray[i], weight.get(charArray[i]) + (1d / depth));
+                depth--;
+            } else {
+                depth++;
+            }
+        }
+
+        Map.Entry<Character, Double> max = null;
+        for (Map.Entry<Character, Double> entry : weight.entrySet()) {
+            if (max == null || max.getValue() < entry.getValue()) {
+                max = entry;
+            }
+        }
+
+        System.out.println(max.getKey());
+    }
+}
