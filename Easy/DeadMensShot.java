ef3ba6919f773b7e47a8f7932e95c32e3df1302b
gctom <gctom57@gmail.com>
2019-05-24 17:27:51 +0200

    https://www.codingame.com/training/easy/dead-mens-shot

--- Easy/DeadMensShot.java
+++ Easy/DeadMensShot.java
@@ -8,7 +8,7 @@ import java.awt.Polygon;
  **/
 class Solution {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         final Polygon polygon = new Polygon();
