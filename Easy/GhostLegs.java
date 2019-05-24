5db12ce1dcaa1361b31779615cf30fb9f80e85f3
gctom <gctom57@gmail.com>
2019-05-24 12:54:38 +0200

    https://www.codingame.com/training/easy/ghost-legs

---
+++ Easy/GhostLegs.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.util.stream.IntStream;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    private static String[][] diag;
+    // Position of first line of array and current position
+    private static Map<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int W = in.nextInt();
+        int H = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+
+        List<String> ROWS = new ArrayList();
+
+        for (int i = 0; i < H; i++) {
+            ROWS.add(in.nextLine());
+        }
+
+        IntStream.range(0, W).filter(j -> j % 3 == 0).forEach(j -> {
+
+            int position = j;
+            Character character = 'A';
+
+            for (int i = 0; i < ROWS.size(); i++) {
+                if (i == 0) {
+                    character = ROWS.get(i).charAt(position);
+                }
+
+                if (position < W - 1 && ROWS.get(i).charAt(position + 1) == '-') {
+                    position += 3;
+                } else if (position >= 1 && ROWS.get(i).charAt(position - 1) == '-') {
+                    position -= 3;
+                }
+            }
+
+            System.out.println(character + "" + ROWS.get(H - 1).charAt(position));
+        });
+
+    }
+}
