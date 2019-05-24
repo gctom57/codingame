baecd9002b7794d677e43446970a30502914a10b
gctom <gctom57@gmail.com>
2019-05-24 13:24:41 +0200

    https://www.codingame.com/training/easy/lumen

---
+++ Easy/Lumen.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int L = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+
+        boolean[][] lighted = new boolean[N][N];
+
+        int nbDarkSpot = N * N;
+
+        // lines
+        for (int i = 0; i < N; i++) {
+            String[] line = in.nextLine().split(" ");
+
+            int nbLights = 0;
+            // rows
+            for (int j = 0; j < N; j++) {
+                if (line[j].equals("C")) {
+                    for (int k = Math.max(0, i - L + 1); k < Math.min(N, i + L); k++) {
+                        for (int l = Math.max(0, j - L + 1); l < Math.min(N, j + L); l++) {
+                            if (!lighted[k][l]) {
+                                lighted[k][l] = true;
+                                nbLights++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            nbDarkSpot -= nbLights;
+        }
+
+        System.out.println(nbDarkSpot);
+    }
+}
