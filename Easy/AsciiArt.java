30f142d4df636b2b3af84484e623cd69e9ad664e
gctom <gctom57@gmail.com>
2019-05-24 12:16:35 +0200

    https://www.codingame.com/training/easy/ascii-art

---
+++ Easy/AsciiArt.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+    static Map<String, List<String>> asciiArts = new HashMap();
+
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int L = in.nextInt();
+        int H = in.nextInt();
+        if (in.hasNextLine()) {
+            in.nextLine();
+        }
+        String text = in.nextLine().toUpperCase();
+
+        for (int i = 0; i < H; i++) {
+            String ROW = in.nextLine();
+
+            for(int j = 0; j < text.length(); j++) {
+                int delta = text.charAt(j) - 65; // Ascii code for 'A'
+                if (delta < 0 || delta >= 26) {
+                    delta = 26;
+                }
+
+                System.out.print(ROW.substring(L * delta, L * (delta + 1)));
+            }
+            System.out.println();
+        }
+    }
+}
